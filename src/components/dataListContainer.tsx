04f6a434c7766929e308c0ee8a26ca2b2b28be2b
CarusoMac <caruso.macd@gmail.com>
2023-03-27 12:49:18 +0200

    current location and current cell

--- src/components/dataListContainer.tsx
+++ src/components/dataListContainer.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import LogInfo from './logInfo';
 
+
 const numberOfActiveLogs = 3; //fetch z db
 //fetch z DB bude dotaz na DB na vsechny logy zobrazi se okno s datem vytvoreni a jmenem logu + datum dne ze ktereho ten log je, po odskrtnuti checkboxu a submitu se nahrajou logy do jsonu
 
 // z vytvoreneho jsonu bude probihat zobrazovani a vypocty ve frontendu
 
-export default function DataListContainer() {
+interface DataListContainerProps {
+  currentTime: number;
+}
+
+
+export default function DataListContainer(props: DataListContainerProps) {
   const [logsIDarr, setLogsIDarr] = useState(['2Ad', 'B', 'C']); //info vytazene z vytvoreneho jsonu
   const [LogDeleteId, setLogDeleteId] = useState('');
 
@@ -18,10 +24,12 @@ export default function DataListContainer() {
     }
   }, [LogDeleteId]);
 
+
+
   return (
     <div className='col-3 datalist-container'>
       {/* {logsIDarr.map((logID) => <LogInfo logID={logID} onDeleteLog={setLogsIDarr} />)} */}
-      {logsIDarr.map((logID) => <LogInfo key={logID} logID={logID} onLogDelete={setLogDeleteId} />)}
+      {logsIDarr.map((logID) => <LogInfo key={logID} logID={logID} onLogDelete={setLogDeleteId} currentTime={props.currentTime} />)}
     </div>
   );
 }
