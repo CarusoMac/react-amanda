952854a1033a0b6ec528b16633b4f4c651d92edc
CarusoMac <caruso.macd@gmail.com>
2023-03-30 09:38:02 +0200

    client-side validation

--- src/components/logInForm.tsx
+++ src/components/logInForm.tsx
@@ -1,5 +1,6 @@
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import React from 'react'
+import * as Yup from 'yup'
 
 interface FormValues {
   user: string;
@@ -20,15 +21,28 @@ export default function LogInForm() {
     <div className='container'>
 
       <h3>Přihlášení</h3>
-      <Formik initialValues={InitValues} onSubmit={onSubmit}>
+      <Formik
+        initialValues={InitValues}
+        onSubmit={onSubmit}
+        validationSchema={Yup.object().shape({
+          user: Yup.string().required('Nezadali jste uživatelské jméno'),
+          password: Yup.string().required('Nezadali jste heslo')
+        })}
+      >
         <Form>
           <div className='form-group'>
             <label htmlFor="user">Uživatelské jméno</label>
             <Field name="user" id="user" className="form-control" />
             <label htmlFor="password">Heslo</label>
             <Field name="password" id="password" className="form-control" />
+            <ErrorMessage name="user">
+              {msg => <div className='text-danger'>{msg}</div>}
+            </ErrorMessage>
+            <ErrorMessage name="password">
+              {msg => <div className='text-danger'>{msg}</div>}
+            </ErrorMessage>
           </div>
-          <button className='file-submit-btn'>
+          <button className='file-submit-btn' type='submit'>
             Potvrdit
           </button>
         </Form>
