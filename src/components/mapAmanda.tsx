7a88a1b8a7518788bf45dbf95247a93d692d76d8
CarusoMac <caruso.macd@gmail.com>
2023-04-05 12:37:55 +0200

    uprava kodu po napojeni na DB

--- src/components/mapAmanda.tsx
+++ src/components/mapAmanda.tsx
@@ -6,52 +6,73 @@ import { useRef, useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../App.css';
 //data
-import log1 from '../data/log1.json'
+
 
 //components
 import DragZnacka from './dragZnacka';
 import { marker } from 'leaflet';
+import { LogInfoDTO } from '../DTOs/logInfoDTO';
 
 interface MapComponentProps {
   currentTime: number;
   onMarkerChange: (position: [number, number]) => void;
+  dataList: LogInfoDTO[][];
 }
+
+//vypocteni mapoveho centra
+const mapcenter = (logs: LogInfoDTO[][]) => {
+  let pathCoordinates: [number, number][] = [];
+  logs.forEach(log => {
+    log.forEach(location => {
+      pathCoordinates.push([location.lat, location.lon]);
+    });
+  });
+  return pathCoordinates.reduce(
+    (acc, cur) => [acc[0] + cur[0] / pathCoordinates.length, acc[1] + cur[1] / pathCoordinates.length],
+    [0, 0]
+  );
+};
 
 export default function MapComponent(props: MapComponentProps) {
 
-  const [currentLocation, setCurrentLocation] = useState([log1[log1.length - 1].lat, log1[log1.length - 1].lon]);
-  const [activeTower, setActiveTower] = useState(log1[log1.length - 1].cellid);
-  const [pathInTime, setPathInTime] = useState<[number, number][]>([]);
+  // const [currentLocation, setCurrentLocation] = useState([log1[log1.length - 1].lat, log1[log1.length - 1].lon]);
+  const [pathInTimeCollection, setPathInTimeCollection] = useState<[number, number][][]>([]);
+  const [mapCenter, setMapCenter] = useState<[number, number]>([0, 0]);
+  const [markerLocation, setMarkerLocation] = useState(mapCenter)
 
-
-  const mapcenter = () => {
-    let pathCoordinates: [number, number][] = [];
-    log1.map(location => {
-      pathCoordinates.push([location.lat, location.lon]);
-    });
-    return pathCoordinates.reduce(
-      (acc, cur) => [acc[0] + cur[0] / pathCoordinates.length, acc[1] + cur[1] / pathCoordinates.length],
-      [0, 0]
-    )
-  }
-  const center = mapcenter();
-  const [markerLocation, setMarkerLocation] = useState(mapcenter())
   useEffect(() => {
-    let tempPathInTime: [number, number][] = [];
-    log1.forEach(location => {
-      if ((location.timestamp) <= props.currentTime) {
-        tempPathInTime.push([location.lat, location.lon]);
-        setActiveTower(location.cellid);
-      }
-    });
-    setPathInTime(tempPathInTime);
-    setCurrentLocation(tempPathInTime[tempPathInTime.length - 1]);
-    //setDistance
+    const newCenter = mapcenter(props.dataList);
+    console.log("map center" + newCenter)
+    setMapCenter(newCenter);
+  }, [props.dataList]);
+
+  //test passing props - ok
+  // useEffect(() => {
+  //   console.log(props.dataList);
+  // }, [props.dataList]);
+
+
+  useEffect(() => {
+    let tempPathInTimeCollection: [number, number][][] = [];
+    let dataList = props.dataList;
+    dataList.forEach(data => {
+      let tempPathInTime: [number, number][] = [];
+
+      data.forEach(location => {
+        if ((location.measured_at) <= props.currentTime) {
+          tempPathInTime.push([location.lat, location.lon]);
+        }
+      });
+      tempPathInTimeCollection.push(tempPathInTime);
+      setPathInTimeCollection(tempPathInTimeCollection);
+
+    })
   }, [props.currentTime]);
+
+
 
   useEffect(() => {
     props.onMarkerChange(markerLocation);
-    //setDistance
   }, [markerLocation]);
 
 
@@ -61,14 +82,22 @@ export default function MapComponent(props: MapComponentProps) {
     <>
       <div className='col-9'>
         {/* <MapContainer center={mapcenter()} zoom={15} scrollWheelZoom={true} > */}
-        <MapContainer center={mapcenter()} zoom={15} scrollWheelZoom={true} >
+        <MapContainer key={mapCenter.toString()} center={mapCenter} zoom={15} scrollWheelZoom={true} >
 
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <DragZnacka positions={center} onPositionChange={setMarkerLocation} />
-          <Polyline positions={pathInTime} color="red" />
+          <DragZnacka positions={mapCenter} onPositionChange={setMarkerLocation} />
+
+          <LayerGroup>
+            {pathInTimeCollection.map(path => (
+              <Polyline positions={path} color="red" />
+            ))}
+          </LayerGroup>
+
+
+
         </MapContainer>
       </div>
     </>
